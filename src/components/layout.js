666378c117380b1850cdae4015369133cd90852e
monognuisy <ysmsmart1@gmail.com>
2021-11-21 22:19:25 +0900

    chore: new plugin for code highlighting

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,9 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import { defineCustomElements as deckDeckGoHighlightElement } from "@deckdeckgo/highlight-code/dist/loader"
+deckDeckGoHighlightElement()
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
