40739e5d600847592780f2c27f8dc6f001730c8e
monognuisy <ysmsmart1@gmail.com>
2021-11-23 17:29:15 +0900

    style: add empty lines

--- src/templates/tags-post.tsx
+++ src/templates/tags-post.tsx
@@ -50,7 +50,7 @@ const Tags = ({ pageContext, data }) => {
               This links to a page that does not yet exist.
               You'll come back to it!
             */}
-      <Link to="/tags">All tags</Link>
+      <Link to="/tags">↑ All tags</Link>
     </div>
   )
 }
