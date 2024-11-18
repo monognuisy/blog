f5e7fad19968be848bc9fac38c65d62d11bd9b6a
monognuisy <ysmsmart1@gmail.com>
2024-11-18 11:36:57 +0900

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const isProduction = process.env.NODE_ENV === 'production';
-// const basePath = isProduction ? '/blog' : '';
 const basePath = '';
 
 const nextConfig = {
