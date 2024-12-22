5e85e1b190ff6bd3deb2689f3aa41c47c57a00d6
monognuisy <ysmsmart1@gmail.com>
2024-12-22 16:06:50 +0900

    feat: make copy button visible on hover

--- src/app/_components/post/codeblock/Pre.tsx
+++ src/app/_components/post/codeblock/Pre.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { DetailedHTMLProps, HTMLAttributes, useState } from 'react';
 import CopyButton from './CopyButton';
 
@@ -11,21 +9,10 @@ export type TPreProps = DetailedHTMLProps<
 };
 
 const Pre = ({ raw, children, ...props }: TPreProps) => {
-  const [isCodeClicked, setIsCodeClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsCodeClicked(true);
-    setTimeout(() => {
-      setIsCodeClicked(false);
-    }, 5000);
-  };
-
   return (
-    <div className="relative">
-      <pre {...props} onClick={handleClick}>
-        {children}
-      </pre>
-      {isCodeClicked && <CopyButton raw={raw} />}
+    <div className="relative group">
+      <pre {...props}>{children}</pre>
+      <CopyButton raw={raw} />
     </div>
   );
 };
