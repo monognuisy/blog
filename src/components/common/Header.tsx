6d2fcf2db2e80a7f59435422a13893f2850059a6
monognuisy <ysmsmart1@gmail.com>
2025-06-04 13:50:08 +0900

    feat: increase width

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <header className="dark:bg-dark-bg/50 w-full bg-white/50 shadow-lg backdrop-blur-md sticky top-0 left-0 z-20">
-      <div className="flex py-3 max-w-[1200px] px-4 mx-auto">
+      <div className="flex py-3 max-w-[80rem] 2xl:max-w-[96rem] px-4 mx-auto">
         <section className="flex-grow flex items-center gap-3">
           <Link href={'/'} style={{ minWidth: logoSize, minHeight: logoSize }}>
             <Image
